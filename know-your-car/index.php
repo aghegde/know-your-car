d6a21d597e37d1b3b6c5bb10fcc68ec2c8ca8f6f
aghegde <20844851+aghegde@users.noreply.github.com>
2021-11-05 12:50:30 +0530

    Update index.php

--- know-your-car/index.php
+++ know-your-car/index.php
@@ -6,8 +6,7 @@
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <h1>Welcome to my page</h1>
+        <h1>Know your Car</h1>
         <p>You will never find another place as cool as this</p>
-        <p><img src="img/bridge.jpg" alt="Know your Car"/></p>
     </body>
 </html>
