93f529ee8478b14f0156ba3c8030c01c99d7200f
aghegde <aghegd@gmail.com>
2021-11-19 23:00:41 +0530

    aaaaxas

--- know-your-car/index.php
+++ know-your-car/index.php
@@ -1,12 +1,31 @@
-<!doctype html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title>My wonderful website</title>
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <h1>Know your Car</h1>
-        <p>You will never find another place as cool as this</p>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Bootstrap Example</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>My First Bootstrap Page</h1>
+  </div>
+  <div class="row">
+    <div class="col-sm-4">
+      ...
+    </div>
+    <div class="col-sm-4">
+      ...
+    </div>
+    <div class="col-sm-4">
+    ...
+    </div>
+  </div>
+</div>
+
+</body>
 </html>
