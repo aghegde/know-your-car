239342d4759b27328f61316a7c941b035e77cecd
aghegde <aghegd@gmail.com>
2021-11-19 23:33:31 +0530

    aaaaxas

--- know-your-car/index.php
+++ know-your-car/index.php
@@ -12,36 +12,103 @@
 <script src="know-your-car/car.js"></script>
 </head>
 <body>
+	  <h1>Elegant Contact Form.</h1>
+   <div class="info"><a href="https://www.grandvincent-marion.fr" target="_blank"><p> Made with <i class="fa fa-heart"></i> by Marion Grandvincent </p></a></div>
 
-<header>
-  <h1>CAR 4U</h1>
-</header>
+  <form>
+	    <h1>Should you have any questions, please do not hesitate to contact me :</h1>
 
-<div id="form">
+    <div class="contentform">
+    	<div id="sendmessage"> Your message has been sent successfully. Thank you. </div>
 
-<div class="fish" id="fish"></div>
-<div class="fish" id="fish2"></div>
 
-<form id="waterform" method="post">
+    	<div class="leftcontact">
+			      <div class="form-group">
+			        <p>Surname<span>*</span></p>
+			        <span class="icon-case"><i class="fa fa-male"></i></span>
+				        <input type="text" name="nom" id="nom" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Nom' doit être renseigné."/>
+                <div class="validation"></div>
+       </div>
 
-<div class="formgroup" id="name-form">
-    <label for="name">Your name*</label>
-    <input type="text" id="name" name="name" />
-</div>
+            <div class="form-group">
+            <p>Name <span>*</span></p>
+            <span class="icon-case"><i class="fa fa-user"></i></span>
+				<input type="text" name="prenom" id="prenom" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Prénom' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
 
-<div class="formgroup" id="email-form">
-    <label for="email">Your e-mail*</label>
-    <input type="email" id="email" name="email" />
-</div>
+			<div class="form-group">
+			<p>E-mail <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-envelope-o"></i></span>
+                <input type="email" name="email" id="email" data-rule="email" data-msg="Vérifiez votre saisie sur les champs : Le champ 'E-mail' est obligatoire."/>
+                <div class="validation"></div>
+			</div>
 
-<div class="formgroup" id="message-form">
-    <label for="message">Your message</label>
-    <textarea id="message" name="message"></textarea>
-</div>
+			<div class="form-group">
+			<p>Company <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-home"></i></span>
+				<input type="text" name="society" id="society" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Société' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
 
-  <input type="submit" value="Send your message!" />
+			<div class="form-group">
+			<p>Company Address <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-location-arrow"></i></span>
+				<input type="text" name="adresse" id="adresse" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Adresse' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
+
+			<div class="form-group">
+			<p>Postcode <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-map-marker"></i></span>
+				<input type="text" name="postal" id="postal" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Code postal' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
+
+
+
+	</div>
+
+	<div class="rightcontact">
+
+			<div class="form-group">
+			<p>City <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-building-o"></i></span>
+				<input type="text" name="ville" id="ville" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Ville' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
+
+			<div class="form-group">
+			<p>Phone number <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-phone"></i></span>
+				<input type="text" name="phone" id="phone" data-rule="maxlen:10" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Téléphone' doit être renseigné. Minimum 10 chiffres"/>
+                <div class="validation"></div>
+			</div>
+
+			<div class="form-group">
+			<p>Function <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-info"></i></span>
+                <input type="text" name="fonction" id="fonction" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Fonction' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
+
+			<div class="form-group">
+			<p>Subject <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-comment-o"></i></span>
+                <input type="text" name="sujet" id="sujet" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Sujet' doit être renseigné."/>
+                <div class="validation"></div>
+			</div>
+
+			<div class="form-group">
+			<p>Message <span>*</span></p>
+			<span class="icon-case"><i class="fa fa-comments-o"></i></span>
+                <textarea name="message" rows="14" data-rule="required" data-msg="Vérifiez votre saisie sur les champs : Le champ 'Message' doit être renseigné."></textarea>
+                <div class="validation"></div>
+			</div>
+	</div>
+	</div>
+<button type="submit" class="bouton-contact">Send</button>
+
 </form>
-</div>
-
 </body>
 </html>
