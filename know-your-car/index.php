1522790dc18f695d40091000e87e59e9621cabe3
aghegde <aghegd@gmail.com>
2021-11-19 23:30:59 +0530

    aaaaxas

--- know-your-car/index.php
+++ know-your-car/index.php
@@ -7,9 +7,9 @@
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
-<link rel="stylesheet" href="site.css">
+<link rel="stylesheet" href="know-your-car/site.css">
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
-<script src="car.js"></script>
+<script src="know-your-car/car.js"></script>
 </head>
 <body>
 
